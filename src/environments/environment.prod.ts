17f2f25cc12794aa1b15c7308be92f24dbae77fb
Swapnil Patel <patels6840@uhcl.edu>
2022-03-27 10:25:42 -0500

    reverted JSON parsing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
-let va = process.env['FIREBASE_KEY'] ? process.env['FIREBASE_KEY'] : '';
+// let va = process.env['FIREBASE_KEY'] ? process.env['FIREBASE_KEY'] : '';
 
 export const environment = {
   production: true,
   version: 1.2,
   instance: 'Production',
-  firebase: JSON.parse(va),
+  firebase: process.env['FIREBASE_KEY'],
 };
