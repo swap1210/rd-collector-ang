e778569d05ca12f43cbd11beb8fe31f42b52a64f
Swapnil Patel <patels6840@uhcl.edu>
2022-03-27 08:49:26 -0500

    polyfill added

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,6 +58,8 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js'; // Included with Angular CLI.
+import * as process from 'process';
+window['process'] = process;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
